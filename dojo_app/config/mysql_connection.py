c90f4f6b1448fe9683eac3338c930eeb6c823d9a
Ethan Jones <ethan08jones@gmail.com>
2023-08-13 11:59:32 -0400

    finished dojo_and_ninjas

--- dojo_app/config/mysql_connection.py
+++ dojo_app/config/mysql_connection.py
@@ -9,7 +9,7 @@ class MySQLConnection:
         connection = pymysql.connect(
             host="localhost",
             user="root",
-            password="rootroot",
+            password="root",
             db=db,
             charset="utf8mb4",
             cursorclass=pymysql.cursors.DictCursor,
